29ff39a91fbb05377ca65785efa3505a112a26e8
MrBowser <mitchbowser1990@gmail.com>
2021-11-26 16:22:02 -0800

    v1

--- jsmain.js
+++ jsmain.js
@@ -1,4 +1,5 @@
 
+//canvas hero image, creating 30 bouncing balls in a canvas object
 
 const canvas = document.getElementById('hero');
 const ctx = canvas.getContext('2d');
@@ -24,9 +25,9 @@ let allBall = [];
 
 for(let i=0;i<10;i++)
 {
-    allBall.push(new Ball(Math.random()* w, Math.random()* h,  Math.random() *8,  Math.random() *5, Math.random()* 50+5, "#222121"));
-    allBall.push(new Ball(Math.random()* w, Math.random()* h,  Math.random() *5,  Math.random() *8, Math.random()* 50+5, "#457b9d"));
-    allBall.push(new Ball(Math.random()* w, Math.random()* h,  Math.random() *5,  Math.random() *5, Math.random()* 50+5, "#e63946"));
+    allBall.push(new Ball(Math.random()* w, Math.random()* h,  (Math.random() *6)-3,  (Math.random() *4)-2, Math.random()* 50+5, "#222121"));
+    allBall.push(new Ball(Math.random()* w, Math.random()* h,  (Math.random() *4)-2,  (Math.random() *6)-3, Math.random()* 50+5, "#457b9d"));
+    allBall.push(new Ball(Math.random()* w, Math.random()* h,  (Math.random() *4)-2,  (Math.random() *4)-2, Math.random()* 50+5, "#e63946"));
 
 }
 
@@ -57,7 +58,89 @@ function draw() {
 
 draw();
 
+//adding intersection observer for in view animation triggers for education and work sections
 
+let education = document.getElementById("intertrig")
+
+let work = document.getElementById("trig2");
+
+let fadeinArray = [education,work];
+
+
+
+function Intered (entries)
+{
+  entries.forEach((entry) =>
+    {
+      if (entry.isIntersecting=== true )
+    {
+      entry.target.classList.add("visible");
+      console.log("thisworks");
+    }
+    else
+    {
+      entry.target.classList.remove("visible");
+      console.log("broken");  
+    }
+
+    });
+    
+};
+
+let observer = new IntersectionObserver(Intered);
+
+fadeinArray.forEach(ele => {
+  observer.observe(ele);
+});
+
+
+//initial switch
+
+changeWord();
+
+// flashing text in the header
+setInterval(changeWord,4000);
+
+
+function changeWord() {
+  const heroWord = document.getElementById("rotatehero");
+  heroWord.style.opacity ="0";
+  
+
+  if(heroWord.innerText ==="Developer")
+  {
+    heroWord.className ="InOut";
+    heroWord.innerText ="Engineer";
+    setTimeout (function () {
+      heroWord.classList.remove("InOut")},3300
+      );
+    
+
+  }
+  else if(heroWord.innerText==="Engineer")
+  {
+    heroWord.className ="InOut";
+    heroWord.innerText="Researcher";
+    setTimeout (function () {
+    heroWord.classList.remove("InOut")},3300
+    );
+    
+  }
+  else if(heroWord.innerText==="Researcher")
+  {
+    heroWord.className ="InOut";
+    heroWord.innerText ="Developer";
+    setTimeout (function () {
+      heroWord.classList.remove("InOut")},3300
+      );
+    
+  }
+  else{
+    console.log("this broke");
+    
+  }
+
+};
 
 
 
